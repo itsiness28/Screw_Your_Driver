4707aa6046e8f78a298046ccbfd568a4877bfb10
Inés Castaño Gijón <146824804+itsiness28@users.noreply.github.com>
2026-02-07 15:24:25 +0100

    Ciclo de turnos, temporizador,resumen de turno
    Queda arreglar el shift overview. Preguntar como se ha planteado el cursor ya que al haber componentes de UI (botón9 requieren del click para funcionar pero al ser 3d, no hay cursor

--- Screw_Your_Driver_1APersona/Assets/Scripts/ShiftController.cs
+++ Screw_Your_Driver_1APersona/Assets/Scripts/ShiftController.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class ShiftController : MonoBehaviour
 {
+
+    public bool shiftOn = false;
+    [SerializeField] public float timerVar;
+    [SerializeField] public GameObject canvasShiftOverview;
+    [SerializeField] public Button closeShiftOverview;
+
+    public TextMeshProUGUI shiftCountdown;
+    public TextMeshProUGUI uiShiftIndicator;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        uiShiftIndicator.text = "Shop Closed";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UIShiftChange();
+        closeShiftOverview.onClick.AddListener(HideShiftOverview); //esto no anda bien
+
     }
+
+
+    private void UIShiftChange()
+    {
+        if (shiftOn)
+        {
+            uiShiftIndicator.text = "Shop Open";
+            Temporizador();
+            if (timerVar == 0)
+            {
+                timerVar = 90;
+            }
+        }
+        else
+        {
+            uiShiftIndicator.text = "Shop Closed";
+            canvasShiftOverview.SetActive(true);//esto tengo que revisarlo para que la primera vez no salga e incluirle un objeto, una imagen, que al ahcer click en ella vuelva a ocultarse.
+        }
+    }
+
+    private void Temporizador()
+    {
+        timerVar -= Time.deltaTime;
+        int minutes = Mathf.FloorToInt(timerVar / 60);
+        int seconds = Mathf.FloorToInt(timerVar % 60);
+        shiftCountdown.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        if (timerVar <= 0)
+        {
+            timerVar = 0;
+            shiftCountdown.text = "0:00";
+            shiftOn = false;
+        }
+    }
+
+    private void HideShiftOverview()
+    {
+        canvasShiftOverview.SetActive(false);
+    }
+
+
 }
